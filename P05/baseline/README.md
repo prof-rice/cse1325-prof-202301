5abdfdceba77c045ce77983b83c09157ca23a10b
Professor Rice <george.rice@uta.edu>
2023-02-22 15:45:00 -0600

    P05 baseline code

---
+++ P05/baseline/README.md
@@ -0,0 +1,7 @@
+# Support Code for Exceptional Laptops and Supercomputers Always (ELSA)
+
+This baseline includes the Java code to implement the ELSA Store class, as well as a TestStore.java regression test.
+
+**This code will NOT compile until paired with your own Customer, Option, Computer, and Order classes!** See the class diagram in the requirements document for details.
+
+The intent is for you to copy these files to your own repository to test your code AFTER it has been written.
---
+++ P05/baseline/Store.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public class Store {
+    public Store(String name) {
+        this.name = name;
+    }
+    public String name() {
+        return this.name;
+    }
+    
+    // ///////////////////////////////////////////////////////////
+    // Customers
+    
+    public void add(Customer customer) {
+        if(!customers.contains(customer)) customers.add(customer);
+    }
+    public Object[] customers() {
+        return this.customers.toArray();
+    }
+    
+    // ///////////////////////////////////////////////////////////
+    // Options
+    
+    public void add(Option option) {
+        if(!options.contains(option)) options.add(option);
+    }
+    public Object[] options() {
+        return this.options.toArray();
+    }
+    
+    // ///////////////////////////////////////////////////////////
+    // Computers
+    
+    public void add(Computer computer) {
+        if(!computers.contains(computer)) computers.add(computer);
+    }
+    public Object[] computers() {
+        return this.computers.toArray();
+    }
+    
+    // ///////////////////////////////////////////////////////////
+    // Orders
+    
+    public void add(Order order) {
+        if(!orders.contains(order)) orders.add(order);
+    }
+    public Object[] orders() {
+        return this.orders.toArray();
+    }
+
+    // ///////////////////////////////////////////////////////////
+    // Fields
+    
+    private String name;
+    private ArrayList<Customer> customers = new ArrayList<>();
+    private ArrayList<Option> options = new ArrayList<>();
+    private ArrayList<Computer> computers = new ArrayList<>();
+    private ArrayList<Order> orders = new ArrayList<>();
+}
